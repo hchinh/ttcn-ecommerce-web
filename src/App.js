1ecab3896da70b70d3dee532171eecf4c9d4184d
Chinh Huynh <chinh.huynh@team.enouvo.com>
2022-01-03 09:02:15 +0700

    setup base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import AdminFeature from 'features/Admin';
+import LoginPage from 'features/Auth/pages/LoginPage';
+import CartFeature from 'features/Cart';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
-
+import ErrorPage from './components/ErrorPage/ErrorPage';
+import UserLoginPage from './features/Auth/pages/UserLoginPage';
+import Product from './features/Product/product.jsx';
+import ProductDetail from './features/ProductDetail/ProductDetail';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route path='/' exact component={Product} />
+        <Route path='/login1' component={UserLoginPage} />
+        <Route path='/login' component={LoginPage} />
+        <Route path='/admin' component={AdminFeature} />
+        <Route path='/cart' component={CartFeature} />
+        <Route path='/productDetails/:id' exact component={ProductDetail} />
+        <Route component={ErrorPage} />
+      </Switch>
     </div>
   );
 }
